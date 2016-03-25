9fd49fb3343fbb366d19588c4cd23aecc0032fd2
jose nazario <jose.monkey.org@gmail.com>
2016-03-25 09:16:50 -0400

    fix

--- PERSIST-1/index.php
+++ PERSIST-1/index.php
@@ -1,24 +1,33 @@
 <?php     include("../common/header.php");   ?>
 
 <?php
-if (strlen($_POST["comment"]) > 0) {
-    file_put_contents("comments.txt", $_POST["comment"] + "\n\n----==\n\n", FILE_APPEND);
+if (isset($_POST["comment"])) {
+    $data = $_POST["comment"] . "\n\n----==\n\n";
+    $ret = file_put_contents("comments.txt", $data, FILE_APPEND | LOCK_EX);
+    if ($ret === false) {
+         die('There was an error writing this file');
+    }
 } 
 
+hint("this should be obvious");
+
 echo "<h2>Comments</h2>";
-$txt = file_get_contents("comments.txt");
+if (file_exists("comments.txt") == TRUE) {
+    $txt = file_get_contents("comments.txt");    
+} else {
+    $txt = "";
+}
 if ($txt != FALSE) {
     $comments = explode("----==", $txt);
-    echo var_dump($comments);
-    foreach ($commments as $comment) {
-        echo sprintf("<p>%s</p><hr />\n", $comment);
+    for ($i = 0; $i < count($comments); $i++) {
+        echo "<p>$comments[$i]</p><hr />\n";
     }
 }
 ?>
 <p>
-<form method="POST" action="/PERSIST-1/index.php">
+<form method="POST" action="index.php">
     comment? <br/>
     <input type="textarea" rows="4" cols="90" name="comment">
-    <button type="submit" form="form1" value="Submit">Submit</button>
+    <input type="submit" name="submit" value="Save Data">
 </form>
 </p>  
